4d2ba8a27d1f165cd75f2156b719c35e761ac5c6
Ai2healthcare <105121687+Ai2healthcare@users.noreply.github.com>
2023-06-20 21:12:46 -0700

    Create glm.nb.R

---
+++ R/glm.nb.R
@@ -0,0 +1,25 @@
+require(foreign)
+require(ggplot2)
+require(MASS)
+
+dat <- read.dta("https://stats.idre.ucla.edu/stat/stata/dae/nb_data.dta")
+dat <- within(dat, {
+  prog <- factor(prog, levels = 1:3, labels = c("General", "Academic", "Vocational"))
+  id <- factor(id)
+})
+
+summary(dat)
+
+ggplot(dat, aes(daysabs, fill = prog)) + geom_histogram(binwidth = 1) + facet_grid(prog ~ 
+                                                                                     ., margins = TRUE, scales = "free")
+
+with(dat, tapply(daysabs, prog, function(x) {
+  sprintf("M (SD) = %1.2f (%1.2f)", mean(x), sd(x))
+}))
+
+table(dat$prog) 
+library("tidyverse")
+dat<-dat %>% filter(grepl("General|Academic",prog))
+summary(m1 <- glm.nb(daysabs ~ prog, data = dat))
+
+t.test(daysabs ~ prog,dat)
